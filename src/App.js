3250287306eab2cf6504927ee4f598aa341d8582
Acosta Ledesma Martín Sebastián <martinacosta190@gmail.com>
2023-10-28 19:41:31 -0300

    Se agregaron botones para establecer una condicion True o False

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React,{useState} from 'react';
 function App() {
 const [notas, setNotas]=useState([]);
 const [nuevaNota, setNuevaNota]=useState('');
+const [condicionNota, setCondicionNota]=useState(true);
 const agregarNota=()=>{
-  if (nuevaNota!=='') {
+  //Con este If requerimos que la nota siempre tenga como condición un True
+  if (nuevaNota!=='' && condicionNota==true) {
     setNotas([...notas, nuevaNota]);
     setNuevaNota('');
+    setCondicionNota(true);
   }
 };
 const eliminarNota=(index)=>{
@@ -18,6 +21,10 @@ return (<div>
   <h1>Lista de Notas</h1>
   <input type='text' placeholder='Nueva Nota' value={nuevaNota} onChange={(e)=>setNuevaNota(e.target.value)}></input>
   <button onClick={agregarNota}>Agregar Nota</button>
+  {/* Si presionamos el botón false antes de agreagar una nota, no se agrega
+  Solo se agregará si pulsamos el botón True */}
+  <button onClick={()=>setCondicionNota(true)}>True</button>
+  <button onClick={()=>setCondicionNota(false)}>False</button>
   <ul>{notas.map((nota,index)=>(
     <li key={index}>
       <img src='./img/disparo.png'></img>
